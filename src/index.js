574c0d1156a04e91eca533c2aac74963201f43dd
Cj Diamond <mrcjdiamond@gmail.com>
2016-03-11 15:38:22 +1100

    misc changes

--- src/index.js
+++ src/index.js
@@ -6,26 +6,27 @@ var app = module.exports = framework();
 // start the web server
 app.start = function() {
   return app.listen(function() {
-    app.emit('started');
     var baseUrl = app.get('url').replace(/\/$/, '');
+
+    app.emit('started');
     console.log('Web server listening at: %s', baseUrl);
+
     if (app.get('loopback-component-explorer')) {
       var explorerPath = app.get('loopback-component-explorer').mountPath;
       console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
     }
+
   });
 };
 
 // Bootstrap the application, configure models, datasources and middleware.
-app.boot = function() {
-  return boot(app, __dirname, function(err) {
+boot(app, __dirname, function(err) {
     if (err) throw err;
 
     // start the server if `$ node server.js`
     if (require.main === module)
       app.start();
-  });
-};
+});
 
 app.boot();
 
